4cbfc8849b88c597376cd13e46b90ce4c54db758
Yuki Tada <yuki0125.bass@gmail.com>
2025-06-26 00:28:34 +0900

    react

--- src/lib/microcms.ts
+++ src/lib/microcms.ts
@@ -7,20 +7,29 @@ const client = createClient({
 });
 
 // 型定義
+// スケジュールの型
 export type Schedule = {
   title: string;
   content: string;
 } & MicroCMSListContent;
 
+// カテゴリの型
+export type Category = {
+  name: string;
+} & MicroCMSListContent;
+
 // APIの呼び出し
+// スケジュール一覧を取得
 export const getSchedules = async (queries?: MicroCMSQueries) => {
   return await client.getList<Schedule>({ endpoint: "schedules", queries });
 };
 
-export const getScheduleDetail = async (
-  contentId: string,
-  queries?: MicroCMSQueries
-) => {
+// カテゴリ一覧を取得
+export const getCategories = async (queries?: MicroCMSQueries) => {
+  return await client.getList<Category>({ endpoint: "categories", queries });
+};
+
+export const getScheduleDetail = async (contentId: string, queries?: MicroCMSQueries) => {
   return await client.getListDetail<Schedule>({
     endpoint: "schedules",
     contentId,
